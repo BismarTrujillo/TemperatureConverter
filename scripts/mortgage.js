5dcf46e90effd71598dd530c259caad05d9784b0
Bismar Trujillo <bismar20@hotmail.com>
2022-10-23 13:05:42 -0400

    mortgage fixed

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -20,26 +20,8 @@ function calculateBtnOnClick() {
     let yearsAsNumber = Number(yearsField.value);
     //variables for clean formula 
     let interestMontlyPercent = interestAsNumber / 100 / 12;
-<<<<<<< HEAD
-    let yearsInMoths = yearsAsNumber * 12;
-    //Mortgage formula --> M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1].
-
-    //let moPayment = loanAsNumber*(interestMontlyPercent * (1 + interestMontlyPercent) ^ (yearsInMoths)) / ((1 + interestMontlyPercent) ^ (yearsInMoths) - 1);
-    let moPayment = loanAsNumber * (interestMontlyPercent * (Math.pow(1 + interestMontlyPercent, yearsInMoths))) / (Math.pow(1+interestMontlyPercent,yearsInMoths)-1);
-    //function monthlyPayment(p, n, i) {
-  //eturn p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
-//}
-   
-console.log(moPayment + " mo pament");
-console.log(loanAsNumber + " loan as number");
-console.log(interestMontlyPercent + " insterest montly percent");
-console.log(yearsInMoths + " years in months");
-
-const mopaymentField = document.getElementById("mopaymentField");
-
-    mopaymentField.value = moPayment.toFixed(2);
-=======
     let yearsInMonths = yearsAsNumber * 12;
+
     //Mortgage formula --> M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1].
     let moPayment = loanAsNumber * (interestMontlyPercent * (Math.pow(1 + interestMontlyPercent, yearsInMonths))) / (Math.pow(1+interestMontlyPercent,yearsInMonths)-1);
     let totalInterest = (moPayment * yearsInMonths) - loanAsNumber;
@@ -49,6 +31,5 @@ const mopaymentField = document.getElementById("mopaymentField");
     
     mopaymentField.value = Math.round(moPayment*100)/100;
     totalInterestField.value = Math.round(totalInterest*100)/100;
->>>>>>> mortgage
 
 }
