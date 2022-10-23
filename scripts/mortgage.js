880aee470ed0c1bf97973525e26e3e864eaddb0e
Bismar Trujillo <bismar20@hotmail.com>
2022-10-23 12:43:28 -0400

    mortgage formula fixed

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -19,13 +19,18 @@ function calculateBtnOnClick() {
     let interestAsNumber = Number(interestField.value);
     let yearsAsNumber = Number(yearsField.value);
     //variables for clean formula 
-    let interestMontlyPercent = interestAsNumber * (100 / 12);
+    let interestMontlyPercent = interestAsNumber / 100 / 12;
     let yearsInMoths = yearsAsNumber * 12;
     //Mortgage formula --> M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1].
-    let moPayment = loanAsNumber(interestMontlyPercent * (1 + interestMontlyPercent) ^ (yearsInMoths)) / ((1 + interestMontlyPercent) ^ (yearsInMoths) - 1);
+    let moPayment = loanAsNumber * (interestMontlyPercent * (Math.pow(1 + interestMontlyPercent, yearsInMoths))) / (Math.pow(1+interestMontlyPercent,yearsInMoths)-1);
 
+    //let moPayment = loanAsNumber*(interestMontlyPercent * (1 + interestMontlyPercent) ^ (yearsInMoths)) / ((1 + interestMontlyPercent) ^ (yearsInMoths) - 1);
+    let totalInterest = loanAsNumber - (moPayment * yearsInMoths);
+    
     const mopaymentField = document.getElementById("mopaymentField");
-
-    mopaymentField.value = moPayment;
+    const totalInterestField = document.getElementById("interestField");
+    
+    mopaymentField.value = Math.round(moPayment*100)/100;
+    totalInterestField.value = Math.round(totalInterest*100)/100;
 
 }
